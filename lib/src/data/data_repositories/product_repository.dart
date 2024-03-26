9d209826a3b9449d4eef410efee1e5393eea3f4e
7Arthit <artsshit12@gmail.com>
2024-03-26 22:44:25 +0700

    app search product & select type

--- lib/src/data/data_repositories/product_repository.dart
+++ lib/src/data/data_repositories/product_repository.dart
@@ -1,4 +1,7 @@
 import 'package:computer_shop_app/src/config/app_storage.dart';
+import 'package:computer_shop_app/src/data/data_models/request/search_request.dart';
+import 'package:computer_shop_app/src/domain/entities/search/search.dart';
+import 'package:computer_shop_app/src/domain/entities/type/type_drop_downs.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
@@ -26,6 +29,67 @@ class DataProductRepository extends ProductRepository {
           page_size: 2000,
         );
         return Right(response.data.data);
+    } on NotFoundException {
+      return const Left(Failure(message: "occurred error!"));
+    } on DioException catch (e) {
+      final response = Failure.fromJson(e.response!.data);
+      if (e.response?.statusCode == 401) {
+        return Left(UnAuthorizeFailure(
+          code: response.code ?? e.response?.statusCode.toString(),
+          errors: response.errors,
+          message: response.message,
+          statusCode: e.response?.statusCode,
+        ));
+      }
+      return Left(response);
+    }
+  }
+
+  @override
+  Future<Either<Failure, Search>> getSearchProductList({
+    String? productName,
+    int? typeIdProducts
+  }) async {
+    try {
+      final token = await AppStorage.shared.getToken();
+      final response = await _client.searchs(
+        accessToken: "Bearer $token",
+        form: SearchRequest(
+            pageIndex: 1,
+            pageSize: 2000,
+            sortField: '',
+            sortOrder: -1,
+            criteria: Criteria(
+              productName: productName ?? '',
+              productTypeId: typeIdProducts ?? 1,
+            ),
+        ),
+      );
+      return Right(response.data.data);
+    } on NotFoundException {
+      return const Left(Failure(message: "occurred error!"));
+    } on DioException catch (e) {
+      final response = Failure.fromJson(e.response!.data);
+      if (e.response?.statusCode == 401) {
+        return Left(UnAuthorizeFailure(
+          code: response.code ?? e.response?.statusCode.toString(),
+          errors: response.errors,
+          message: response.message,
+          statusCode: e.response?.statusCode,
+        ));
+      }
+      return Left(response);
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<TypeDropDowns>>> getProductTypeList() async {
+    try {
+      final token = await AppStorage.shared.getToken();
+      final response = await _client.productType(
+          accessToken: "Bearer $token",
+      );
+      return Right(response.data.data);
     } on NotFoundException {
       return const Left(Failure(message: "occurred error!"));
     } on DioException catch (e) {
--- lib/src/domain/repositories/product_repository.dart
+++ lib/src/domain/repositories/product_repository.dart
@@ -1,3 +1,6 @@
+import 'package:computer_shop_app/src/domain/entities/role/role_drop_downs.dart';
+import 'package:computer_shop_app/src/domain/entities/search/search.dart';
+import 'package:computer_shop_app/src/domain/entities/type/type_drop_downs.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:computer_shop_app/src/data/data_sources/helper/failure.dart';
@@ -6,4 +9,8 @@ import 'package:computer_shop_app/src/domain/entities/product/product.dart';
 abstract class ProductRepository {
 
   Future<Either<Failure, List<Product>>> getProductList();
+
+  Future<Either<Failure, Search>> getSearchProductList({String? productName, int? typeIdProducts});
+
+  Future<Either<Failure, List<TypeDropDowns>>> getProductTypeList();
 }
